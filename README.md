a7b6a933ff75822a48a7ea07172fd64e1bed9b8b
codermagefox <codermagefox@codermagefox.com>
2018-12-05 20:33:58 +0800

    ver 0.0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+#AflredFE
+可能是最好的前端Aflred工作流.
+Ver 0.0.1
---
+++ VariableNameTransLate/littleUnderLine.js
@@ -0,0 +1,9 @@
+//LittleUnderLine主逻辑入口
+
+const Config = require('./Config/CustomerConfig.js');
+const GetResult = require('./MainLogic/GetResultFromYOUDAO.js');
+const {Processer} = require('./MainLogic/ProcessingResults.js');
+
+(function () {
+    GetResult.core(Config.youDaoApi,Config.getParams(),Processer.littleUnderLine);
+})();
