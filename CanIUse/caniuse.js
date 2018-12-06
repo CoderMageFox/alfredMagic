9a9c92c6d19cdaa0d4621d5c4212c8b6684e5bef
codermagefox <codermagefox@codermagefox.com>
2018-12-06 23:14:55 +0800

    Add MDN SearchFeature,Some Todo.

--- CanIUse/caniuse.js
+++ CanIUse/caniuse.js
@@ -1,16 +1,21 @@
 const alfy = require('alfy');
 const CanIUse = require('caniuse-api');
-const _ = require('lodash')
 class FeachCanIuseAction {
     constructor(){}
+    //主寻找逻辑
     findName(input){
         let alfyResultArr = [];
         let inputGuess = CanIUse.find(input);
+        //此逻辑用于判断精准查询,代码层面可后续优化
+        if (input === CanIUse.find(input)){
+            alfyResultArr.push(this.preciseQuery(input))
+        }else
         inputGuess.map((item)=>{
             alfyResultArr.push(this.preciseQuery(item))
         });
         alfy.output(alfyResultArr)
     }
+    //格式化拿到的数据
     FormatResults(CompatibilityResult){
         let ChromeSupportVersion,FirefoxSupportVersion,IESupportVersion,SafariSupportVersion,ResultString;
         ChromeSupportVersion = `${CompatibilityResult['chrome']['y'] || `暂无数据`}`;
@@ -20,6 +25,7 @@ class FeachCanIuseAction {
         ResultString = `最低兼容版本 Chrome:${ChromeSupportVersion} FireFox:${FirefoxSupportVersion} IE:${IESupportVersion} Safari:${SafariSupportVersion}`
         return ResultString
     };
+    //精准查询
     preciseQuery(input){
         let response =  CanIUse.getSupport(input);
         let FormattedResult = this.FormatResults(response);
